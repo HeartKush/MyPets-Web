3ce33a0a43a331b893eaa2cde3afda660719a59b
syduarte <syduarte@uniguajira.edu.co>
2022-07-17 23:59:25 -0500

    add aside bar

--- src/app/shared/aside-dashboard/aside-dashboard.component.ts
+++ src/app/shared/aside-dashboard/aside-dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aside-dashboard',
   templateUrl: './aside-dashboard.component.html',
-  styleUrls: ['./aside-dashboard.component.scss']
+  styleUrls: ['../navbar-dashboard/navbar-dashboard.component.scss']
 })
 export class AsideDashboardComponent implements OnInit {
 
